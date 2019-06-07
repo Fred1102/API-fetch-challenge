3b4e147b0a682a44011949166d50c9d2581b0487
AlexandraMadalina <alexandramadalinacosma@gmail.com>
2019-06-07 11:33:51 +0200

    next and prev

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const container = document.getElementById('container');
 makeBeersCards();
 
 function getBeers(page) {
-    return fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=40`)
+    return fetch(`https://api.punkapi.com/v2/beers?page=${page}&per_page=50`)
         .then(resp => resp.json());
 }
 
@@ -17,13 +17,17 @@ async function makeBeersCards() {
     for (let i = 1; i <= beerArr.length; ++i) {
         const column = document.createElement('div');
         column.classList.add('col');
+        let imageIUrl = beerArr[i - 1].image_url;
+        if (beerArr[i - 1].image_url == null) {
+            imageIUrl = "https://images.punkapi.com/v2/keg.png";
+        }
         column.innerHTML = `<div class="card" style="width: 18rem;">
-    <img src="${beerArr[i-1].image_url}" class="card-img-top" alt="#">
-    <div class="card-body text-center">
-      <h5 class="card-title">${beerArr[i-1].name}</h5>
-      <p class="card-text text-justify">${beerArr[i-1].description}</p>
-    </div>
-    </div>`;
+                                <img src="${imageIUrl}" class="card-img-top" alt="#">
+                                <div class="card-body text-center">
+                                    <h5 class="card-title">${beerArr[i-1].name}</h5>
+                                    <p class="card-text text-justify">${beerArr[i-1].description}</p>
+                                </div>
+                            </div>`;
         row.appendChild(column);
         if (i % 5 == 0) {
             container.appendChild(row);
@@ -36,13 +40,27 @@ async function makeBeersCards() {
 }
 const paginationLinks = document.getElementsByClassName('page-link');
 
-for (let i = 1; i < paginationLinks.length - 1; ++i) {
+for (let i = 0; i < paginationLinks.length; ++i) {
+    if(i==0){
+        paginationLinks[i].onclick = () => {
+            if()
+           --nextPage;
+           makeBeersCards();
+        }
+    }
+    if(i== paginationLinks.length-1){
+        paginationLinks[i].onclick = () => {
+            ++nextPage;
+            makeBeersCards();
+        }
+    }
     paginationLinks[i].onclick = e => {
         const number = e.target.textContent;
         nextPage = parseInt(number);
-        container.innerHTML="";
+        container.innerHTML = "";
         makeBeersCards();
-        console.log(number);
+        console.log(nextPage);
     }
 }
+
 console.log(paginationLinks);
